277b1ce53fa0ce76d001e49f9e33f14e41f0c986
lolo-12 <pai0102@hotmail.com>
2019-11-26 19:47:08 +0800

    update loading

---
+++ pageloading/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+
+gulp.task('pug', function buildHTML() {
+    return gulp.src('./source/*.pug')
+        .pipe(plumber())
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./source/scss/**/*.sass')
+        .pipe(plumber())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./source/scss/**/*.sass', gulp.series('sass'));
+    gulp.watch('./source/*.pug', gulp.series('pug'));
+});
+
+gulp.task('default', gulp.series('pug', 'sass', 'watch'));
---
+++ pageloading/public/js/all.js
@@ -0,0 +1,15 @@
+var percent = 0
+
+function eatCount() {
+    $(".monsterText").html("We are<br>Fairydust!")
+}
+
+var timer = setInterval(function () {
+    $(".bar").css("width", percent + "%")
+    percent += 1;
+    if (percent >= 100) {
+        $(".pageloading").addClass("complete");
+        setTimeout(eatCount, 3000);
+        clearInterval(timer);
+    }
+}, 30)
